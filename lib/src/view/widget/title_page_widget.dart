4d077f8bf21ae28707704467bd2657a1f588894d
rafaelcr123 <rafaelcruz1@hotmail.com>
2021-10-03 19:20:51 -0300

    feat: add title page widget

---
+++ lib/src/view/widget/title_page_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:pokeapp/src/core/app_colors.dart';
+
+class TitlePageWidget extends StatelessWidget {
+  final String titleText;
+
+  const TitlePageWidget({Key? key, required this.titleText}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(
+          children: [
+            Text(
+              this.titleText,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: AppColors.secondary),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
