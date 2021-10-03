3a63a4a4f629585a34b6a7c2ab44d3c6fb91ccb5
rafaelcr123 <rafaelcruz1@hotmail.com>
2021-10-03 00:42:50 -0300

    feat: add delay to catalogPage

--- lib/src/view/screen/splash/splash_page.dart
+++ lib/src/view/screen/splash/splash_page.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:pokeapp/src/core/app_colors.dart';
 import 'package:pokeapp/src/core/app_images.dart';
+import 'package:pokeapp/src/view/screen/catalog/catalog_page.dart';
 
 class SplashPage extends StatelessWidget {
-  const SplashPage({ Key? key }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container (
-        decoration: BoxDecoration(
-          color: AppColors.white
+    Future.delayed(Duration(seconds: 3)).then(
+      (_) => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => CatalogPage(),
         ),
-        child: Center (
-          child: Image.asset(
-            AppImages.logo,
-            width: 120,
-            fit: BoxFit.fill 
-          )
+      ),
+    );
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(color: AppColors.white),
+        child: Center(
+            child: Image.asset(
+              AppImages.logo, 
+              width: 120, 
+              fit: BoxFit.fill,
+            ),
         ),
       ),
     );
