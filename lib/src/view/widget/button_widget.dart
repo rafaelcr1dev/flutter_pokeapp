1dbba56e273cca33b142439027bb7ac3968194a1
rafaelcr123 <rafaelcruz1@hotmail.com>
2021-10-04 18:16:27 -0300

    refactor: add onPressed and btn without icon

--- lib/src/view/widget/button_widget.dart
+++ lib/src/view/widget/button_widget.dart
@@ -8,44 +8,66 @@ class ButtonWidget extends StatelessWidget {
   final IconData icon;
   final Color backgroundColor;
   final Color fontColor;
+  final GestureTapCallback onPressed;
+  final bool hasIcon;
 
   const ButtonWidget({
-    Key? key, 
+    Key? key,
     this.fontSize = 14,
     this.iconSize = 20,
     this.backgroundColor = Colors.transparent,
+    this.hasIcon = true,
+    this.icon = Icons.filter,
+    required this.onPressed,
     required this.textButton,
-    required this.icon,
     required this.fontColor,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return OutlinedButton.icon(
-      style: OutlinedButton.styleFrom(
-          minimumSize: Size(double.infinity, 30),
-          padding: EdgeInsets.symmetric(vertical: 14),
-          side: BorderSide(
-            width: 1.0, 
-            color: this.backgroundColor,
+    if (this.hasIcon) {
+      return OutlinedButton.icon(
+          style: OutlinedButton.styleFrom(
+            minimumSize: Size(double.infinity, 30),
+            padding: EdgeInsets.symmetric(vertical: 14),
+            side: BorderSide(
+              width: 1.0,
+              color: this.backgroundColor,
+            ),
+            backgroundColor: this.backgroundColor,
           ),
-          backgroundColor: this.backgroundColor,
-      ),
-      icon: Icon(
-        this.icon,
-        color: this.fontColor,
-        size: this.iconSize,
-      ),
-      label: Text(
-        this.textButton,
-        style: TextStyle(
-          color: this.fontColor, 
-          fontSize: this.fontSize,
-        ),
-      ),
-      onPressed: () {
-        print('Received click');
-      },
-    );
+          icon: Icon(
+            this.icon,
+            color: this.fontColor,
+            size: this.iconSize,
+          ),
+          label: Text(
+            this.textButton,
+            style: TextStyle(
+              color: this.fontColor,
+              fontSize: this.fontSize,
+            ),
+          ),
+          onPressed: onPressed);
+    } else {
+         return OutlinedButton(
+          style: OutlinedButton.styleFrom(
+            minimumSize: Size(double.infinity, 30),
+            padding: EdgeInsets.symmetric(vertical: 14),
+            side: BorderSide(
+              width: 1.0,
+              color: this.backgroundColor,
+            ),
+            backgroundColor: this.backgroundColor,
+          ),
+          child: Text(
+            this.textButton,
+            style: TextStyle(
+              color: this.fontColor,
+              fontSize: this.fontSize,
+            ),
+          ),
+          onPressed: onPressed);
+    }
   }
 }
