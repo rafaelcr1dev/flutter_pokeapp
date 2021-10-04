c3b10e19f39f8933e791d67a2f4aa3de4b478478
rafaelcr123 <rafaelcruz1@hotmail.com>
2021-10-04 18:13:24 -0300

    refactor: indent code and insert onPressed on btn

--- lib/src/view/screen/cart/cart_page.dart
+++ lib/src/view/screen/cart/cart_page.dart
@@ -3,11 +3,12 @@ import 'package:pokeapp/src/core/app_colors.dart';
 import 'package:pokeapp/src/view/widget/app_bar_widget.dart';
 import 'package:pokeapp/src/view/widget/button_widget.dart';
 import 'package:pokeapp/src/view/widget/item_widget.dart';
+import 'package:pokeapp/src/view/widget/modal_widget.dart';
 import 'package:pokeapp/src/view/widget/title_page_widget.dart';
 import 'package:pokeapp/src/view/widget/total_results_widget.dart';
 
 class CartPage extends StatelessWidget {
-  const CartPage({ Key? key }) : super(key: key);
+  const CartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,36 +19,43 @@ class CartPage extends StatelessWidget {
           horizontal: 14,
           vertical: 10,
         ),
-        child: Column(children: <Widget>[ 
-          TitlePageWidget(
-            titleText: "Carrinho"
-          ),
+        child: Column(children: <Widget>[
+          TitlePageWidget(titleText: "Carrinho"),
           SizedBox(height: 4),
-          TotalResultsWidget(
-            total: 20
-          ),
+          TotalResultsWidget(total: 20),
           SizedBox(height: 14),
           Expanded(
-            child: ListView.builder(
-              itemCount: 20,
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                return ItemWidget(
-                  item: ""
-                );
-              },
-            )
-          ),
+              child: ListView.builder(
+            itemCount: 20,
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              return ItemWidget(item: "");
+            },
+          )),
           SizedBox(height: 14),
           ButtonWidget(
-            textButton: "Finalizar pedido",
-            icon: Icons.shopping_cart,
-            backgroundColor: AppColors.primary,
-            fontColor: Colors.white,
-            iconSize: 16,
-            fontSize: 14
-          ),
-        ]),
+              textButton: "Finalizar pedido",
+              icon: Icons.shopping_cart,
+              backgroundColor: AppColors.primary,
+              fontColor: Colors.white,
+              iconSize: 16,
+              fontSize: 14,
+              onPressed: () {
+                showModalBottomSheet<void>(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return ModalWidget(
+                      title: "Pedido finalizado",
+                      description: "O número do seu pedido é #010101",
+                      textButton: "Ir para a página inicial",
+                      onPressedCallback: () => Navigator.pop(context),
+                    );
+                  },
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
