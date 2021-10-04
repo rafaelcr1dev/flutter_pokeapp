49e99e3a02913cddbbfac416f649ae617215620d
rafaelcr123 <rafaelcruz1@hotmail.com>
2021-10-03 23:36:23 -0300

    feat: add image fake pokemon.png

--- lib/src/core/app_images.dart
+++ lib/src/core/app_images.dart
@@ -1,5 +1,6 @@
 class AppImages {
     static String get logo => "assets/images/logo.png";
+    static String get pokemon => "assets/images/pokemon.png";
     static String get check => "assets/images/check.png";
     static String get error => "assets/images/error.png";
 }
