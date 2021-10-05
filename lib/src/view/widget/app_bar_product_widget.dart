2f0e3f256b996ecf5291f9d70b1ba6005e6ebc7d
rafaelcr123 <rafaelcruz1@hotmail.com>
2021-10-05 14:35:12 -0300

    refactor: change the struct to StatelessWidget

--- lib/src/view/widget/app_bar_product_widget.dart
+++ lib/src/view/widget/app_bar_product_widget.dart
@@ -2,33 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:pokeapp/src/core/app_colors.dart';
 import 'package:pokeapp/src/core/app_images.dart';
 
-class AppBarProductWidget extends PreferredSize{
-  AppBarProductWidget()
-      : super(
-          preferredSize: Size.fromHeight(54),
-          child: AppBar(
-            shape: Border(
-              bottom: BorderSide(
-                color: AppColors.primary, 
-                width: 0,
-              ),
+class AppBarProductWidget extends StatelessWidget
+    implements PreferredSizeWidget {
+  final bool hasBackButton;
+  const AppBarProductWidget({Key? key, this.hasBackButton = false})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      shape: Border(
+        bottom: BorderSide(
+          color: AppColors.primary,
+          width: 0,
+        ),
+      ),
+      shadowColor: AppColors.primary,
+      leading: IconButton(
+        icon: Icon(
+          Icons.arrow_back,
+          color: Colors.white,
+        ),
+        onPressed: () => Navigator.of(context).pop(),
+      ),
+      backgroundColor: AppColors.primary,
+      bottomOpacity: 0,
+      elevation: 0.0,
+      actions: [
+        IconButton(
+            icon: Icon(
+              Icons.favorite_border,
+              color: Colors.white,
             ),
-            shadowColor: AppColors.primary,
-            leading: Container(
-              padding: const EdgeInsets.only(left: 10),
-              child: Image.asset(AppImages.logo),
-            ),
-            backgroundColor: AppColors.primary,
-            bottomOpacity: 0,
-            elevation: 0.0,
-            actions: [
-              IconButton(icon: Icon(
-                Icons.favorite_border,
-                color: Colors.white,
-              ),
-                onPressed: () => {}
-              )
-            ],
-          )
+            onPressed: () => {})
+      ],
     );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
